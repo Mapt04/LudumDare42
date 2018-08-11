b076b8a98101960dc6c3f2487dfd9c0ec4dde7ec
Matias Apt <maptib04@gmail.com>
2018-08-11 12:52:55 -0400

    Added Black Hole, Exit, Game Over Screen, Level Menu and fixed minor organization issues

---
+++ LudumDare42Entry/Assets/Scripts/BlackHole.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHole : MonoBehaviour {
+    public float growSpeed;
+    Vector3 scale;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scale = transform.localScale;
+        scale.x += growSpeed * Time.deltaTime;
+        scale.y += growSpeed * Time.deltaTime;
+        transform.localScale = scale;
+	}
+}
---
+++ LudumDare42Entry/Assets/Scripts/LoseScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseScript : MonoBehaviour {
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("BlackHole"))
+        {
+            FindObjectOfType<GameManager>().LoseGame();
+        }
+    }
+}
