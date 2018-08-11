2b0ebd2a32115315d0797a0ee8b458410df97eed
Matias Apt <maptib04@gmail.com>
2018-08-11 10:39:21 -0400

    Added Key Counter System

---
+++ LudumDare42Entry/Assets/Scripts/KeysCounterText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeysCounterText : MonoBehaviour
+{
+    public int keys;
+    public Text keyCounter;
+
+    void Update()
+    {
+        keyCounter.text = (keys + "/4");
+    }
+}
---
+++ LudumDare42Entry/Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour {
+    private KeysCounterText kct;
+    private void Start()
+    {
+        kct = GameObject.FindGameObjectWithTag("KeyCounter").GetComponent<KeysCounterText>();
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Hit");
+        if (col.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            kct.keys += 1;
+        }
+    }
+
+}
